dbaa49cf731a458281f22d4afc6b0db6dc2c89b7
Nick Tollett <tollettn@gmail.com>
2015-10-15 15:41:31 -0700

    Added a new class for sprites, and draws the player character on the screen.

--- CaveStory/source/src/game.cpp
+++ CaveStory/source/src/game.cpp
@@ -28,6 +28,8 @@ void Game::gameLoop() {
 	Input input;
 	SDL_Event event;
 
+	this->_player = Sprite(graphics, "content/sprites/MyChar.png", 0, 0, 16, 16, 100, 100);
+
 
 	int LAST_UPDATE_TIME = SDL_GetTicks();
 
@@ -58,11 +60,17 @@ void Game::gameLoop() {
 		int ELAPSED_TIME_MS = CURRENT_TIME_MS - LAST_UPDATE_TIME;
 		this->update(std::min(ELAPSED_TIME_MS, MAX_FRAME_TIME));
 		LAST_UPDATE_TIME = CURRENT_TIME_MS;
+
+		this->draw(graphics);
 	}
 }
 
 void Game::draw(Graphics &graphics) {
+	graphics.clear();
 
+	this->_player.draw(graphics, 100, 100);
+
+	graphics.flip();
 }
 
 void Game::update(float elapsedTime) {
