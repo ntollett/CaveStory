f056c34f78c6293c6992710bf94241b92f15486e
Nick Tollett <tollettn@gmail.com>
2015-10-21 15:29:53 -0700

    Added an AnimatedSprite class that...animates sprites.  Now Quote moves endlessly to the right

---
+++ CaveStory/source/headers/animatedsprite.h
@@ -0,0 +1,82 @@
+#ifndef ANIMATED_SPRITE_H
+#define ANIMATED_SPRITE_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "sprite.h"
+#include "globals.h"
+
+class Graphics;
+
+/* AnimatedSprite class
+ * Holds information for animating sprites
+ */
+
+class AnimatedSprite : public Sprite {
+public:
+	AnimatedSprite();
+	AnimatedSprite(Graphics &graphics, const std::string &filePath, int sourceX, int sourceY, int width, int height,
+		float posX, float posY, float timeToUpdate);
+
+	/* playAnimation
+	 * plays the animation provided if it's not already playing
+	 */
+	void playAnimation(std::string animation, bool once = false);
+
+	/* update
+	 * Updates the animated sprite (timer)
+	 */
+	void update(int elapsedTime);
+
+	/* draw
+	 * Draws the sprite to the screen
+	 */
+	void draw(Graphics &graphics, int x, int y);
+
+	/* setupAnimations
+	 * A required function that sets up all animations for a sprite
+	 */
+	virtual void setupAnimations();
+
+protected:
+	double _timeToUpdate;
+	bool _currentAnimationOnce;
+	std::string _currentAnimation;
+
+	/* addAnimation
+	 * Adds an animation to the map of animations for the sprite
+	 */
+	void addAnimation(int frames, int x, int y, std::string name, int width, int height, Vector2 offset);
+
+	/* resetAnimations
+	 * Resets all animations associated with the sprite
+	 */
+	void resetAnimations();
+
+	/* stopAnimation
+	 * Stops the current animation
+	 */
+	void stopAnimation();
+
+	/* setVisible
+	 * Changes the visibility of the animated sprite
+	 */
+	void setVisible(bool visible);
+
+	/* animationDone
+	 * Logic that happens when an animation ends
+	 */
+	virtual void animationDone(std::string currentAnimation);
+
+private:
+	std::map<std::string, std::vector<SDL_Rect> > _animations;
+	std::map<std::string, Vector2> _offsets;
+
+	int _frameIndex;
+	double _timeElapsed;
+	bool _visible;
+};
+
+#endif
--- CaveStory/source/src/game.cpp
+++ CaveStory/source/src/game.cpp
@@ -28,7 +28,9 @@ void Game::gameLoop() {
 	Input input;
 	SDL_Event event;
 
-	this->_player = Sprite(graphics, "content/sprites/MyChar.png", 0, 0, 16, 16, 100, 100);
+	this->_player = AnimatedSprite(graphics, "content/sprites/MyChar.png", 0, 0, 16, 16, 100, 100, 100);
+	this->_player.setupAnimations();
+	this->_player.playAnimation("RunRight");
 
 
 	int LAST_UPDATE_TIME = SDL_GetTicks();
@@ -74,5 +76,5 @@ void Game::draw(Graphics &graphics) {
 }
 
 void Game::update(float elapsedTime) {
-
+	this->_player.update(elapsedTime);
 }
